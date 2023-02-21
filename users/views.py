10fa874430b93d765fd9438eb72d3d006ede0a77
olegmaslov <oleg.gnome@yandex.ru>
2023-02-21 09:29:30 +0300

    part *

--- users/views.py
+++ users/views.py
@@ -1,14 +1,16 @@
 from rest_framework import generics
 
 from users.models import User
+from users.permissions import OwnProfileEditPermission
 from users.serializers import UserSerializer, UserProfileInfoSerializer
 
 
 class UserUpdateAPIView(generics.UpdateAPIView):
     serializer_class = UserSerializer
     queryset = User.objects.all()
+    permission_classes = [OwnProfileEditPermission]
 
 
 class UserRetrieveAPIView(generics.RetrieveAPIView):
-    serializer_class = UserProfileInfoSerializer
+    serializer_class = UserSerializer
     queryset = User.objects.all()
